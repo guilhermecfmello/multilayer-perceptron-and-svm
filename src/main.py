4fb13348d85d29fc4b7efdde7d85baae169b38b5
Guilherme Mello <guilhermecfmello@gmail.com>
2020-09-28 23:45:02 -0300

    svm ready and working

--- src/main.py
+++ src/main.py
@@ -6,5 +6,8 @@ import utils
 if __name__ == "__main__":
     dataName = utils.getArgs(sys.argv, '-i')
     svm = SVM(dataName)
-    svm.rbf_training()
-    result = svm.evalMetrics()
+    c_list = [1, 1, 0.05]
+    g_list = [1, 0.05, 1]
+    for c,g in zip(c_list,g_list):
+        svm.rbf_training(c=c, gamma=g)
+        result = svm.evalMetrics()
--- src/svm.py
+++ src/svm.py
@@ -1,6 +1,7 @@
 import machine
+import pickle
+import os.path
 import numpy as np
-# from joblib import dump, load
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 
@@ -25,46 +26,81 @@ class SVM(machine.machine):
             newY.append(yAux)
         return newY
 
-    def rbf_training(self):
+    def rbf_training(self, c=0.1, gamma=0.1, k='rbf'):
+        self.c = c
+        self.gamma = gamma
+        self.kernel = k
+        fileName=DATA_FOLDER+'svm_c'+str(c)+'_gamma'+str(gamma)+'_kernel'+str(k)+'.sav'
         print("Runing SVM RBF training...")
         x_train, y_train = super().getDatasetTraining(self.dataPath)
         x_test, y_test = super().getDatasetTest(self.dataPath)
-        c = 0.1
-        gamma = 0.1
-        s = svm.SVC(C=c, kernel='linear', gamma=gamma)
-        y_train2 = self.__vectorToScalar(y_train)
-        s.fit(x_train, y_train2)
+
+        # if exists svm saved
+        if not os.path.isfile(fileName):
+            print('There is not an svm instance saved with this parameters, generating from scretch...')
+            # decision_function_shape: [ovo=n*(n-1)/2,ovr=n]
+            s = svm.SVC(C=c, kernel=k, gamma=gamma, decision_function_shape='ovo')
+            y_train2 = self.__vectorToScalar(y_train)
+            s.fit(x_train, y_train2)
+            print('dumping svm on: ' + fileName)
+            pickle.dump(s, open(fileName, 'wb'))
+        else:
+            print('loading svm from disk on: '+fileName)
+            s = pickle.load(open(fileName, 'rb'))
+        
         y_predict = s.predict(x_test)
         y_predict = self.__scalarToVector(y_predict)
         self.y_predict = y_predict
         self.y_expect = y_test
         return True
-        # print(y_predict)
-        # exit()
 
     def evalMetrics(self):
         print("Evaluating SVM metrics...")
-
+        
         expect = self.__vectorToScalar(self.y_expect)
         predict = self.__vectorToScalar(self.y_predict)
-        print(expect[:30])
-        print(predict[:30])
 
         # vp, vn, fp, fn
         VP, VN, FP, FN = 0, 1, 2, 3
         metrics = np.zeros((10,4))
+        k = 0
         for p, e in zip(predict, expect):
+            e_int = int(e)
+            p_int = int(p)
             # Right prediction, true (verdadeiro alguma coisa)
             if p==e:
-                metrics[e][VP] = metrics[e][VP] + 1
-                metrics[:e][VN] = metrics[:e][VN] + 1
-                metrics[e+1:][VN] = metrics[e+1:][VN] + 1
+                metrics[e_int][VP] = metrics[e_int][VP] + 1
+                metrics.T[VN][:e_int] = metrics.T[VN][:e_int] + 1
+                metrics.T[VN][e_int+1:] = metrics.T[VN][e_int+1:] + 1
             # Wrong prediction, false (falso alguma coisa)
             else:
-                metrics[e][FN] = metrics[e][FN] + 1
-                metrics[p][FP] = metrics[p][FP] + 1
-                for i in range(len(metrics)):
-                    if i != e and i != p:
-                        metrics[i][VN] = metrics[i][VN] + 1
-                
-                       
+                k = k + 1
+                metrics[e_int][FN] = metrics[e_int][FN] + 1
+                metrics[p_int][FP] = metrics[p_int][FP] + 1
+                mi = min([e_int, p_int])
+                ma = max([e_int, p_int])
+                metrics[:mi].T[VN:VN+1] = metrics[:mi].T[VN:VN+1] + 1
+                metrics[mi+1:ma].T[VN:VN+1] = metrics[mi+1:ma].T[VN:VN+1] + 1
+                metrics[ma+1:].T[VN:VN+1] = metrics[ma+1:].T[VN:VN+1] + 1
+        for i in range(10):
+            if metrics[i].sum() != len(self.y_expect):
+                print("Metrics inconsistent")
+        
+        print('========== METRICS ==========')
+        print('SVM Configs: [c='+str(self.c)+', gamma='+str(self.gamma)+', kernel='+str(self.kernel)+']')
+        print(metrics)
+        for i in range(len(metrics)):
+            vp = metrics[i][VP]
+            vn = metrics[i][VN]
+            fp = metrics[i][FP]
+            fn = metrics[i][FN]
+            precision = vp/(vp+fp) if (vp+fp) > 0 else 0
+            recall = vp/(vp+fn) if (vp+fn) > 0 else 0
+            accuracy = (vp+vn)/(vp+fp+fn+vn)
+            f1 = 2*(precision*recall)/(precision+recall) if (precision+recall) > 0 else 0
+            print('For class='+str(i))
+            print('\tPrecision: '+str(precision))
+            print('\tRecall: '+str(recall))
+            print('\tAccuracy: '+str(accuracy))
+            print('\tF-1: '+str(f1))
+        return True
